ac6a1542c730d2664b0e88e53693108ac3e49b5f
DemiOlushola <demmiolu@gmail.com>
2026-08-27 15:40:44 +0200

    opdatering på TouristAttraction og TouristRepository

--- src/main/java/tourism/model/TouristAttraction.java
+++ src/main/java/tourism/model/TouristAttraction.java
@@ -3,4 +3,37 @@ package tourism.model;
 public class TouristAttraction {
     String name;
     String description;
+
+
+    // CONSTRUCTOR
+    public TouristAttraction(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    // GETTERS
+    public String getTouristAttractionName(){
+        return name;
+    }
+
+    public String getTouristAttractionDescription(){
+        return description;
+    }
+
+    // SETTERS
+
+    public String setTouristAttractionName(String name){
+        return this.name = name;
+    }
+
+    public String setTouristAttractionDescription(String description){
+        return this.description = description;
+    }
+
+
+    // TO STRING
+    @Override
+    public String toString(){
+        return "Name: " + this.name + "\n Description: " + this.description
+    }
 }
--- src/main/java/tourism/repository/TouristRepository.java
+++ src/main/java/tourism/repository/TouristRepository.java
@@ -1,8 +1,21 @@
 package tourism.repository;
 
 import org.springframework.stereotype.Repository;
+import tourism.model.TouristAttraction;
 
+import java.util.ArrayList;
+
+@Repository
 public class TouristRepository {
+    ArrayList<TouristAttraction> data = new ArrayList<>();
 
+
+    public TouristRepository(){
+        TouristAttraction touristAttraction1 = new TouristAttraction("Mount Everest", "En kæmpe sten!");
+        TouristAttraction touristAttraction2 = new TouristAttraction("Tsunami", "Pas på herover!");
+
+        data.add(touristAttraction1);
+        data.add(touristAttraction2);
+    }
 
 }
