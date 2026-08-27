b9ee28f0fb5d45bed03ed00883f77cb811c0d537
Yagmur263 <Yace1000@stud.ek.dk>
2026-08-27 14:55:47 +0200

    Initial commit

---
+++ src/main/java/com/example/turistguide1_2/Turistguide12Application.java
@@ -0,0 +1,13 @@
+package com.example.turistguide1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Turistguide12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Turistguide12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/turistguide1_2/Turistguide12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turistguide1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Turistguide12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
